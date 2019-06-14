a53d416dc07c2370c8755ea89efeb52ff8e70825
mibsen <mibsen>
2019-06-14 08:08:44 +0200

    Added turnspeed

--- src/bot/actions/WayPointAction.java
+++ src/bot/actions/WayPointAction.java
@@ -12,16 +12,26 @@ public class WayPointAction implements Action {
 	double x;
 	double y;
 	float speed;
+	private float turnSpeed;
 
 	public WayPointAction(double x, double y, float speed) {
 		this.x = x;
 		this.y = y;
 		this.speed = speed;
+		this.turnSpeed = speed;
 	}
 
+	public WayPointAction(double x, double y, float speed, float turnSpeed) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.turnSpeed = turnSpeed;
+	}
+
+	
 	@Override
 	public void Perform() {
-		Controls.PILOT.setAngularSpeed(Controls.PILOT.getMaxAngularSpeed() * speed);
+		Controls.PILOT.setAngularSpeed(Controls.PILOT.getMaxAngularSpeed() * turnSpeed);
 		Controls.PILOT.setLinearSpeed(Controls.PILOT.getMaxLinearSpeed() * speed);
 		
 		Controls.NAVIGATION.goTo((float) x, (float) y);
