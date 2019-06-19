0fab457a08d53dd7c42a771da72540f4fbb6f026
mibsen <mibsen>
2019-06-19 10:10:15 +0200

    Added grep action

---
+++ src/bot/actions/GrepCollectionAction.java
@@ -0,0 +1,22 @@
+package bot.actions;
+
+import bot.Controls;
+
+public class GrepCollectionAction implements Action{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void Perform() {
+		
+		
+		Controls.COLLECTOR.setSpeed((int)(Controls.COLLECTOR.getMaxSpeed()*0.50));
+
+		Controls.COLLECTOR.rotate(-30);
+		Controls.COLLECTOR.waitComplete();
+	}
+
+}
