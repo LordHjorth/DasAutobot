0cf61751ce44463dd12205cd0209cd0482ae2788
mibsen <mibsen>
2019-06-20 16:11:07 +0200

    Added serial version

--- src/bot/actions/ActionList.java
+++ src/bot/actions/ActionList.java
@@ -2,8 +2,12 @@ package bot.actions;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ActionList extends ArrayList<Action> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1179571801346978329L;
 
 }
